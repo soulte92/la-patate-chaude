88a650284e2ba27aa04e8f42158b913e69fc173f
[Sacha MOUCHON] <[sacha.mouchon@gmail.com]>
2022-12-31 11:09:57 +0100

    initial work on this feature

--- client/app.rs
+++ client/app.rs
@@ -2,16 +2,12 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 
 fn main() {
-    loop {
-
         let mut stream = TcpStream::connect("127.0.0.1:7878").unwrap();
-        let message = "try to speak with";
+        let message = "Hello";
 
         println!("{}", message.len());
         let len = message.len() as u32;
 
         stream.write(&len.to_be_bytes()).unwrap(); // on écrit le préfixe (taille du prochain message)
         stream.write(message.as_bytes()).unwrap(); // puis le message en tant que tel
-
-    }
 }
