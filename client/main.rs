e96530c2f2cc0f5001a9228a480f90af09a70e86
[Sacha MOUCHON] <[sacha.mouchon@gmail.com]>
2023-02-19 19:50:45 +0100

    retrieve work from souleman and add some configuration on the cargo.toml

--- client/main.rs
+++ client/main.rs
@@ -1,60 +1,137 @@
-use std::io::prelude::*;
-use std::net::{TcpStream};
+extern crate random_string;
 
-const ADDRESS_SERVER: &str = "127.0.0.1:7878";
-const HELLO_MESSAGE: &str = r#""Hello""#;
-const SUBSCRIBE_MESSAGE: &str = r#"{"Subscribe":{"name":"Sarah"}}"#;
+use std::borrow::Borrow;
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+mod recover_secret;
+
+use recover_secret::data_structures::{RecoverSecretInput, RecoverSecretOutput};
+use recover_secret::recover_secret_challenge::{Challenge, RecoverSecretChallenge};
 
 fn main() {
-    let stream = TcpStream::connect(ADDRESS_SERVER).unwrap();
-        send_hello_to_server(&stream, HELLO_MESSAGE);
-        send_subscribe_to_server(&stream, SUBSCRIBE_MESSAGE);
-    /* TODO ⬇️
-        while the the response of the server of the server
-        isn't "EndOfGame", i listen the server response.
-     */
-    /*
-        use std::io::Read;
-        use std::net::{TcpListener, TcpStream};
+    let first_test_word = "C'est chou";
+    let recover_secret_output = RecoverSecretOutput{
+        secret_sentence: first_test_word.to_string()
+    };
 
-        fn handle_client(mut stream: TcpStream) {
-            let mut buf_len = [0u8; 4]; // pour lire les 4 octets du u32
-            stream.read_exact(buf_len.as_mut()).unwrap(); // lit exactement la taille du buffer
+    let recover_secret_input = RecoverSecretInput {
+        word_count: 2,
+        letters: "t cCehuCethoCeschouC'schout h".to_string(),
+        tuple_sizes: Vec::from([3, 4, 5, 7, 7, 3])
+    };
 
-            let len = u32::from_le_bytes(buf_len); // convertit les 4 octets en un entier u32
+    let challenge_recover_secret = RecoverSecretChallenge::new(recover_secret_input);
 
-            let mut buf = vec![0u8; len as usize]; // on prépare un buffer pour ce qui va arriver
-            stream.read_exact(buf.as_mut()).unwrap(); // on remplit le buffer
-            // c'est arrivé
-            println!("{buf:?}"); // en octets
-            let s = String::from_utf8_lossy(&buf); // la version loosy n'échoue jamais et nettoie les caractères UTF-8 invalides
-            println!("{s}"); // en String
-        }
+    let status = challenge_recover_secret.verify(&recover_secret_output);
+    let out = challenge_recover_secret.solve();
+    // println!("{}", out.secret_sentence);
+    println!("{}", status);
 
-        fn main() {
-            let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-            for stream in listener.incoming() {
-                handle_client(stream.unwrap());
-            }
-        }
-    */
 }
 
-fn send_hello_to_server(mut stream: &TcpStream, message: &str) {
-    // Print it for us.
-    println!("{}", message.len());
-    // Store the length of our message.
-    let length = message.len() as u32;
-    // We write the prefix which is the size of the next message.
-    stream.write(&length.to_be_bytes()).unwrap();
-    // We write the message.
-    stream.write(message.as_bytes()).unwrap();
-}
+#[cfg(test)]
+mod tests {
+    use ::{RecoverSecretInput, RecoverSecretOutput};
+    use ::{Challenge, RecoverSecretChallenge};
+    use recover_secret::utils::get_value_index;
 
-fn send_subscribe_to_server(mut stream: &TcpStream, message: &str) {
-    println!("{}", message.len());
-    let length= message.len() as u32;
-    stream.write(&length.to_be_bytes()).unwrap();
-    stream.write(message.as_bytes()).unwrap();
+    #[test]
+    fn should_validate_secret_sentence() {
+        // First test
+        let first_test_output = RecoverSecretOutput{
+            secret_sentence: "C'est chou".to_string()
+        };
+
+        let first_test_input = RecoverSecretInput {
+            word_count: 2,
+            letters: "t cCehuCethoCeschouC'schout h".to_string(),
+            tuple_sizes: Vec::from([3, 4, 5, 7, 7, 3])
+        };
+
+        let challenge_recover_secret = RecoverSecretChallenge::new(first_test_input);
+        assert!(challenge_recover_secret.verify(&first_test_output));
+
+        // Second test
+        let second_test_output = RecoverSecretOutput{
+            secret_sentence: "xWRvraj4fonTUmzyO25wA3lBeiM9H".to_string()
+        };
+        let second_test_input = RecoverSecretInput {
+            word_count: 1,
+            letters: "WvyOAlxafUzleiSOl9xayBeHTmy9xWTU5lMW4nUO5lMWRajn2BiHSRUzy5afnUz5wlexWrm5wlBWr4mAlBrUmzHxTUzwlHrfTwBeSRmzlMSRfoUOAe9S4oUiraOiramzM5w3l".to_string(),
+            tuple_sizes: Vec::from([6, 8, 4, 6, 4, 7, 8, 9, 6, 9, 8, 7, 5, 7, 6, 6, 9, 5, 4, 5, 4])
+        };
+        let challenge_recover_secret = RecoverSecretChallenge::new(second_test_input);
+        assert!(challenge_recover_secret.verify(&second_test_output));
+    }
+
+    #[test]
+    fn should_not_validate_secret_sentence() {
+        // First test
+        let first_test_output = RecoverSecretOutput{
+            secret_sentence: "C'est chou".to_string()
+        };
+        let first_test_input = RecoverSecretInput {
+            word_count: 2,
+            letters: "t cCehuCethoCeschouC'schout h".to_string(),
+            tuple_sizes: Vec::from([3, 4, 5, 7, 7, 3])
+        };
+
+        let challenge_recover_secret = RecoverSecretChallenge::new(first_test_input);
+        assert!(challenge_recover_secret.verify(&first_test_output));
+
+        // Second test
+        let second_test_output = RecoverSecretOutput{
+            secret_sentence: "xWRvraj4fonTUmzyO25wA3lBeiM9H".to_string()
+        };
+        let second_test_input = RecoverSecretInput {
+            word_count: 1,
+            letters: "WvyOAlxafUzleiSOl9xayBeHTmy9xWTU5lMW4nUO5lMWRajn2BiHSRUzy5afnUz5wlexWrm5wlBWr4mAlBrUmzHxTUzwlHrfTwBeSRmzlMSRfoUOAe9S4oUiraOiramzM5w3l".to_string(),
+            tuple_sizes: Vec::from([6, 8, 4, 6, 4, 7, 8, 9, 6, 9, 8, 7, 5, 7, 6, 6, 9, 5, 4, 5, 4])
+        };
+        let challenge_recover_secret = RecoverSecretChallenge::new(second_test_input);
+        assert!(challenge_recover_secret.verify(&second_test_output));
+    }
+
+    #[test]
+    fn should_return_good_index_position(){
+        assert_eq!(get_value_index("Hello!".to_string(), "l".to_string()), 2);
+        assert_eq!(get_value_index("Hello!".to_string(), "H".to_string()), 0);
+        assert_eq!(get_value_index("Hello!".to_string(), "o".to_string()), 4);
+    }
+
+    #[test]
+    fn should_not_return_good_index_position(){
+        assert_eq!(get_value_index("Hello!".to_string(), "j".to_string()), -1);
+        assert_eq!(get_value_index("Hello!".to_string(), "1".to_string()), -1);
+    }
+
+    #[test]
+    fn should_generate_good_output1(){
+        let test_input = RecoverSecretInput {
+            word_count: 2,
+            letters: "t cCehuCethoCeschouC'schout h".to_string(),
+            tuple_sizes: Vec::from([3, 4, 5, 7, 7, 3])
+        };
+        let challenge_recover_secret = RecoverSecretChallenge::new(test_input);
+        let mut test_output = challenge_recover_secret.solve();
+
+        assert_eq!(test_output.secret_sentence, "Cet 'schou");
+        assert!(challenge_recover_secret.verify(&test_output));
+    }
+
+    #[test]
+    fn should_generate_good_output2(){
+        let test_input = RecoverSecretInput {
+            word_count: 1,
+            letters: "WvyOAlxafUzleiSOl9xayBeHTmy9xWTU5lMW4nUO5lMWRajn2BiHSRUzy5afnUz5wlexWrm5wlBWr4mAlBrUmzHxTUzwlHrfTwBeSRmzlMSRfoUOAe9S4oUiraOiramzM5w3l".to_string(),
+            tuple_sizes: Vec::from([6, 8, 4, 6, 4, 7, 8, 9, 6, 9, 8, 7, 5, 7, 6, 6, 9, 5, 4, 5, 4])
+        };
+
+        let challenge_recover_secret = RecoverSecretChallenge::new(test_input);
+        let mut test_output = challenge_recover_secret.solve();
+
+        assert_eq!(test_output.secret_sentence, "xWvSRra4fTjnoUmzyO5w3Al2BeiM9H");
+        assert!(challenge_recover_secret.verify(&test_output));
+    }
 }
